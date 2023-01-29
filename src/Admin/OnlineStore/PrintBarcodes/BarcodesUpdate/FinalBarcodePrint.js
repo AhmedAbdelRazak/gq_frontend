eb85c7915476fb5bc918fcdc0ee7fe37d13dd17c
Ahmed Hamouda <ahmedabdelrazzak1001010@gmail.com>
2023-01-28 23:53:47 -0800

    Barcode 8

--- src/Admin/OnlineStore/PrintBarcodes/BarcodesUpdate/FinalBarcodePrint.js
+++ src/Admin/OnlineStore/PrintBarcodes/BarcodesUpdate/FinalBarcodePrint.js
@@ -59,7 +59,7 @@ const FinalBarcodePrint = ({
 											style={{
 												textTransform: "capitalize",
 												// marginTop: i === 0 ? "0px" : "11px",
-												marginBottom: "22px",
+												marginBottom: "25px",
 											}}>
 											<div
 												style={{
